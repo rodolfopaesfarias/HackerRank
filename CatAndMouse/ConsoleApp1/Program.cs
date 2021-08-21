1a53759ae6313886ff5bd66a1cf5d8a8e84abad0
Rodolfo Paes de Farias <rodolfo@movtech.inf.br>
2021-08-21 13:41:52 -0300

    Solved Cat and Mouse Challenge

---
+++ CatAndMouse/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 1;
+            int y = 3;
+            int z = 2;
+
+            Console.WriteLine(catAndMouse(x,y,z));
+            Console.ReadLine();
+        }
+
+
+        // Complete the catAndMouse function below.
+        static string catAndMouse(int x, int y, int z)
+        {
+            int catA = Math.Abs(x - z);
+            int catB = Math.Abs(y - z);
+
+            if (catA < catB)
+            {
+                return "Cat A";
+            } else if (catB < catA)
+            {
+                return "Cat B";
+            }
+            return "Mouse C";
+        }
+    }
+}
