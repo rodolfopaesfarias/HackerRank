421db07e20fce154482d64136433e3d6f27b667b
Rodolfo Paes de Farias <rodolfo@movtech.inf.br>
2021-09-06 11:41:04 -0300

    Solved Designer PDF Viewer Challenge

---
+++ DesignerPDFViewer/DesignerPDFViewer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignerPDFViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> h = new List<int> { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7 };
+            string word = "zaba";
+            Console.WriteLine(designerPdfViewer(h, word));
+            Console.ReadLine();
+        }
+
+        public static int designerPdfViewer(List<int> h, string word)
+        {
+            var letras = word.Distinct();
+            int alturaMaxima = 0;
+            foreach (char letra in letras)
+            {
+                int altura = h[(int)letra - 97];
+                alturaMaxima = Math.Max(altura, alturaMaxima);
+            }
+            return alturaMaxima * word.Length;
+        }
+
+    }
+}
---
+++ HurdleRace/HurdleRace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HurdleRace
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int k = 4;
+            List<int> height = new List<int> { 1, 6, 3, 5, 2 };
+            Console.WriteLine(hurdleRace(k, height));
+
+            k = 7;
+            height = new List<int> { 2, 5, 4, 5, 2 };
+            Console.WriteLine(hurdleRace(k, height));
+
+            Console.ReadKey();
+        }
+
+        public static int hurdleRace(int k, List<int> height)
+        {
+            int maxHeight = height.Max();
+            int potions = maxHeight - k;
+            if (potions < 0) potions = 0;
+            return potions;
+        }
+    }
+}
