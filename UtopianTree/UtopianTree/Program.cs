4ddef64b97a41da94daa694e07a55759d39d9b69
Rodolfo Paes de Farias <rodolfo@movtech.inf.br>
2021-09-07 09:46:20 -0300

    Solving Utopian Tree Challenge Hacker Rank

---
+++ UtopianTree/UtopianTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(utopianTree(0));
+            Console.WriteLine(utopianTree(1));
+            Console.WriteLine(utopianTree(4));
+            Console.ReadLine();
+        }
+
+        public static int utopianTree(int n)
+        {
+            int height = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 != 0) 
+                {
+                    height *= 2;
+                }  else
+                {
+                    height++;
+                }
+            }
+            return height;
+        }
+    }
+}
