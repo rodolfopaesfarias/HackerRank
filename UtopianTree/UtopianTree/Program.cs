d5b4cdcd526fc5f9b95dd8a8d02f7664343f15bd
Rodolfo Paes de Farias <rodolfo@movtech.inf.br>
2021-09-13 21:22:34 -0300

    Utopian Tree

--- UtopianTree/UtopianTree/Program.cs
+++ UtopianTree/UtopianTree/Program.cs
@@ -1,26 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace UtopianTree
 {
     class Program
     {
+
+        static DateTime c;
+        static string d;
         static void Main(string[] args)
         {
             Console.WriteLine(utopianTree(0));
             Console.WriteLine(utopianTree(1));
             Console.WriteLine(utopianTree(4));
             Console.ReadLine();
-        }
+        }        
 
         public static int utopianTree(int n)
         {
             int height = 1;
             for (int i = 1; i <= n; i++)
             {
-                if (i % 2 != 0) 
+                if (i % 2 != 0)
                 {
                     height *= 2;
-                }  else
+                }
+                else
                 {
                     height++;
                 }
